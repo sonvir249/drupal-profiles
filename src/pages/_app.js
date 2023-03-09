23cce4720e6ff3442445bac5e295ab8cb3601d2b
sonvir249 <choudharysonvir@gmail.com>
2023-03-09 14:19:29 +0530

    Metatag updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,12 +10,14 @@ export default function App({ Component, pageProps }) {
         <title>Drupal profile finder</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:site_name" content="drupal-profiles.vercel.app/" />
-        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="Drupal Profile Finder" />
+        <meta name="author" content="sonvir249" />
+        <meta http-equiv="refresh" content="30" />
+        <meta name="keywords" content="Drupal, nextjs, vercel, drupal-profiles, drupal-users, cms"/>
         <meta property="og:url" content="https://drupal-profiles.vercel.app/" />
         <meta property="og:title" content="Drupal Profile Finder" />
-        <meta name="description" content="Drupal is an open source platform for building amazing digital experiences. It's made by a dedicated community. Anyone can use it, and it will always be free." />
-        <meta property="og:description" content="Drupal - the leading open-source CMS for ambitious digital experiences that reach your audience across multiple channels. Because we all have different needs, Drupal allows you to create a unique space in a world of cookie-cutter solutions." />
+        <meta name="description" content="Drupal Profile Finder makes search drupal profiles easy and interactive." />
+        <meta property="og:description" content="Drupal Profile Finder will help you to search profiles with username, first name, last name and country." />
       </Head>
       <Component {...pageProps} />
     </>
