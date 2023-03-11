5a54f949bfaba76db6168b6609c4b42816cb1848
sonvir249 <choudharysonvir@gmail.com>
2023-03-11 14:27:27 +0530

    Fix class attributes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,8 +56,8 @@ export default function Home({ errorCode, profiles }) {
       </Navbar>
       <div className="container">
         <div className='text-center'>
-          <h1 class="mb-1">Drupal Profile Finder</h1>
-          <h6 class="mb-5"><em>Search any profile create on <Link href="https://www.drupal.org/" className={styles.nounderline} target='_blank'>drupal.org</Link></em></h6>
+          <h1 className="mb-1">Drupal Profile Finder</h1>
+          <h6 className="mb-5"><em>Search any profile create on <Link href="https://www.drupal.org/" className={styles.nounderline} target='_blank'>drupal.org</Link></em></h6>
         </div>
         <SearchForm profiles={profiles} />
         <footer className="my-4">
